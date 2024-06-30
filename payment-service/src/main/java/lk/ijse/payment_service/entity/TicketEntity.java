ba1ed840cfaf036c25dd21ee1c8bfd856d97a181
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2024-06-30 19:37:11 +0530

    Created ticket entity

---
+++ payment-service/src/main/java/lk/ijse/payment_service/entity/TicketEntity.java
@@ -0,0 +1,28 @@
+package lk.ijse.payment_service.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ticket")
+public class TicketEntity implements SuperEntity{
+    @Id
+    private String ticketId;
+    private String entranceLocation;
+    private String exitLocation;
+    private String status;
+    private String ticketIssueTime;
+    private Double oneKmPrice;
+    private int totalKm;
+    private Double price;
+
+
+}
