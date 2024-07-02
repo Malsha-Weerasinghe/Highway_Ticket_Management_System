b24dc56f06b82d1dc70a1343a545af78842cf984
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2024-07-02 10:10:14 +0530

    Completed vehicle service

---
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/entity/VehicleEntity.java
@@ -0,0 +1,22 @@
+package lk.ijse.vehicle_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "vehicle")
+public class VehicleEntity implements SuperEntity{
+    @Id
+    private String vehicleId;
+    private String vehicleNumber;
+    private String vehicleType;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")  // Change the column name here
+    private UserEntity users;
+}
